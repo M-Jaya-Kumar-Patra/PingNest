521420d28530f653389856116aef307b99f80064
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2026-07-13 17:30:27 +0530

    redirection from home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,15 @@
-export default function Home() {
-  return (
-    <div>
-      PingNest
-    </div>
-  );
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const cookieStore = await cookies();
+
+  const accessToken =
+    cookieStore.get("accessToken");
+
+  if (accessToken) {
+    redirect("/dashboard");
+  }
+
+  redirect("/login");
 }
