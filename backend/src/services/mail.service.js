ebd4e2d0bb2925b6f00ba14da8c782c612d0b8b2
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2026-07-13 19:08:15 +0530

    log: email env

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -2,21 +2,21 @@ import nodemailer from "nodemailer";
 import { env } from "../config/env.js";
 
 
+
 export const transporter =
   nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+
+    family: 4,
 
     auth: {
       user: env.emailUser,
       pass: env.emailPass,
     },
-
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
   });
+
 
 
 export const sendOtpEmail = async (
